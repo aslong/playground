489dd74f00800c8008bf2c9ea78cf517a6f79234
Andrew Long <aslong87@gmail.com>
2011-12-06 01:12:51 -0800

    Implemented a view for an album.

--- BackboneJS/PeepCode/Basics/backbone-tunes/public/js/Tunes.js
+++ BackboneJS/PeepCode/Basics/backbone-tunes/public/js/Tunes.js
@@ -2,4 +2,16 @@
 
   window.Album = Backbone.Model.extend({});
 
+  window.AlbumView = Backbone.View.extend({
+    initialize: function () {
+      this.template = _.template($('#album-template').html());
+    },
+
+    render: function () {
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      return this;
+    }
+  });
+
 })(jQuery);
